5fbaa96974b664d6624ebac0bb0b68cadb933956
Martim Valente <martimpvalente@gmail.com>
2021-04-28 21:05:07 +0100

    it's done for now. maybe in the future add an install and a js controller or someshit like that

--- README.md
+++
@@ -1,2 +0,0 @@
-# django-component-generator
-i got tired of this bulky modern front-end frameworks and decided to do one myself  ¯\_(ツ)_/¯ 
--- generator.sh
+++ generator.sh
@@ -1,13 +1,30 @@
-get_name() {
-  echo "John"
-}
+#time spent on this: 1,5 hours - still beats Angular
 
-echo -n "Proceed? [y/n]: "
+SUCCESS='\033[0;32m'
+
+echo "please place this file in the base directory of your project"
+echo "this script will fetch the following path: base dir --> templates/components"; echo " "
+
+echo "Proceed? [y/n]"
 read ans
 
-if $ans = n then
-  echo "fix it and come back"
-elif $ans = y then
-  echo  "é bem miudo"
-  siga
+if [[ "$ans" =~ "n" ]]; then
+  echo "exiting..."
+  sleep .5
+  exit
+elif [[ "$ans" =~ "y" ]]; then
+  echo "Insert Component Name"
+  read name
+  sleep .5
+
+  if [[ -d templates/components ]]; then
+    cd templates/components
+    touch $name.html
+    sleep .5
+    echo -e "${SUCCESS}Component $name has been created"
+  else
+    echo "path is incorrect, exiting..."
+    sleep .5
+    exit
+  fi
 fi
